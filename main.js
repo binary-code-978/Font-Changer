2b232d593ec612cb56552674dbcf0ae7163fd911
binary-code-978 <93479009+binary-code-978@users.noreply.github.com>
2022-05-25 15:39:51 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("s")
+
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
